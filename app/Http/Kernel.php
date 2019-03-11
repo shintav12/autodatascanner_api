5967575261ff7e7ebbb525da7fc6921638a9c23e
Manuel Alzamora <manuel.alzamora@ideamultimedia.net>
2019-03-11 16:46:41 -0500

    cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,11 +35,12 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
+            // ...
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
@@ -60,7 +61,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
