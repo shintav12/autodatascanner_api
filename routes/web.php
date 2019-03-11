8167987a255947ccc3f2c0414dc5e5fb4845fc68
Manuel Alzamora <manuel.alzamora@ideamultimedia.net>
2019-03-11 17:07:18 -0500

    middleware

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,23 @@
 
 Route::group(['prefix' => 'api'], function (){
     Route::group(['prefix' => '/brands'], function (){
-        Route::get('/','BrandController@get');
+        Route::get('/','BrandController@get')->middleware('cors');
     });
 
     Route::group(['prefix' => '/systems'], function (){
-        Route::get('','SystemController@get');
-        Route::get('options/{father_slug?}/{system_id?}','SystemController@Options');
-        Route::get('option/{slug}','SystemController@father');
+        Route::get('','SystemController@get')->middleware('cors');
+        Route::get('options/{father_slug?}/{system_id?}','SystemController@Options')->middleware('cors');
+        Route::get('option/{slug}','SystemController@father')->middleware('cors');
     });
 
     Route::group(['prefix' => '/cars'], function (){
-        Route::get('/{brand_id}','CarController@getYears');
-        Route::get('/{brand_id}/{year}','CarController@getModels');
-        Route::get('/{brand_id}/{year}/{model}','CarController@getEngine');
-        Route::get('/{brand_id}/{year}/{model}/{engine}','CarController@getCar');
+        Route::get('/{brand_id}','CarController@getYears')->middleware('cors');
+        Route::get('/{brand_id}/{year}','CarController@getModels')->middleware('cors');
+        Route::get('/{brand_id}/{year}/{model}','CarController@getEngine')->middleware('cors');
+        Route::get('/{brand_id}/{year}/{model}/{engine}','CarController@getCar')->middleware('cors');
     });
     Route::group(['prefix' => '/case'], function (){
-        Route::get('/','CarController@getCase');
+        Route::get('/','CarController@getCase')->middleware('cors');
     });
     
 });
